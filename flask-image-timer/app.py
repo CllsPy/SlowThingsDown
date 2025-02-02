ade545c34399961242f3931aa541f03ac9ac65bf
CLL <96326019+CllsPy@users.noreply.github.com>
2025-02-02 18:08:42 +0000

    update

--- flask-image-timer/app.py
+++ flask-image-timer/app.py
@@ -1,10 +1,19 @@
 from flask import Flask, render_template
+from markupsafe import escape
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/hello')
+def hello():
+    return 'Hello'
+
+@app.route('/user/<name>')
+def show_prof(name):
+    return 'User %s' % escape(name)
 
 if __name__ == '__main__':
     app.run(debug=True)
